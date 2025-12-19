0d9eb6c207b4c2880e8e37a00355b6c17bde17b2
Adrian-Jaron <adrian.jaron@student.vives.be>
2025-12-19 16:34:23 +0100

    created session class inside domain layer
    new class = session
    that class is responsable for study_sessions that u will have during your preparations for exams in january

---
+++ domain/session.py
@@ -0,0 +1,8 @@
+class Session:
+    def __init__(self, id, date, duration, subject_id):
+        self.id = id
+        self.date = date
+        self.duration = duration
+        self.subject_id = subject_id
+        
+        
