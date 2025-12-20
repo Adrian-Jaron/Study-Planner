5d2d43db91243811bb8b243104820760c569edd7
Adrian-Jaron <adrian.jaron@student.vives.be>
2025-12-20 14:25:35 +0100

    deleted wrong files

--- domain/session.py
+++
@@ -1,8 +0,0 @@
-class Session:
-    def __init__(self, id, date, duration, subject_id):
-        self.id = id
-        self.date = date
-        self.duration = duration
-        self.subject_id = subject_id
-        
-        
